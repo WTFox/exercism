6d7bafcd35f8aaf7c579dcb17f6451a4a7193b31
WTFox <Anthonyfox1988@Gmail.com>
2015-11-01 17:53:33 -0600

    2nd iteration of anagram

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,23 +1,28 @@
 # exercism.io anagram detector
 from collections import defaultdict
 
+
 def detect_anagrams(anagram, anagramList):
 	output = []
-	mainWordLetterCount = defaultdict(int)
-	for letter in anagram:
-		mainWordLetterCount[letter.lower()] += 1
 
 	for word in anagramList:
 		if word.lower() == anagram.lower():
 			continue
 
-		listWordLetterCount = defaultdict(int)
-		for letter in word:
-			listWordLetterCount[letter.lower()] += 1
-		if mainWordLetterCount == listWordLetterCount:
+		mainWord = createCounterDict(anagram)
+		listWord = createCounterDict(word)
+		
+		if mainWord == listWord:
 			output.append(word)
-			
+
 	return output
+
+def createCounterDict(word):
+	wordLetterCount = defaultdict(int)
+	for letter in word:
+		wordLetterCount[letter.lower()] += 1
+	return wordLetterCount
+
 
 if __name__ == '__main__':
 	pass
