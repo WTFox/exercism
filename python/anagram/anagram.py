0f5941e029c6f2f2813a2de766bd86f6eba55466
Anthony Fox <anthonyfox1988@gmail.com>
2015-11-02 13:42:02 -0600

    Realised how dumb I was

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,21 +1,10 @@
 # exercism.io anagram detector
-from collections import Counter
 
 
 def detect_anagrams(anagram, anagramList):
-	output = []
-
-	for word in anagramList:
-		if word.lower() == anagram.lower():
-			continue
-
-		mainWord = Counter(anagram.lower())
-		listWord = Counter(word.lower())
-
-		if mainWord == listWord:
-			output.append(word)
-
-	return output
+	return [word for word in anagramList 
+				if not word.lower() == anagram.lower() 
+					and sorted(word.lower()) == sorted(anagram.lower())]
 
 
 if __name__ == '__main__':
