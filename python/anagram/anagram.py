f0d4f8d1af2aecf4fda09fe1b7d05b9eae66cbe5
Anthony Fox <Anthonyfox1988@gmail.com>
2015-11-01 18:41:09 -0800

    3rd iter for anagram

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,5 +1,5 @@
 # exercism.io anagram detector
-from collections import defaultdict
+from collections import Counter
 
 
 def detect_anagrams(anagram, anagramList):
@@ -9,19 +9,13 @@ def detect_anagrams(anagram, anagramList):
 		if word.lower() == anagram.lower():
 			continue
 
-		mainWord = createCounterDict(anagram)
-		listWord = createCounterDict(word)
-		
+		mainWord = Counter(anagram.lower())
+		listWord = Counter(word.lower())
+
 		if mainWord == listWord:
 			output.append(word)
 
 	return output
-
-def createCounterDict(word):
-	wordLetterCount = defaultdict(int)
-	for letter in word:
-		wordLetterCount[letter.lower()] += 1
-	return wordLetterCount
 
 
 if __name__ == '__main__':
