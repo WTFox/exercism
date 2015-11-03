adfd1d881aba002b847b86cc40dd835a4da38f91
Anthony Fox <anthonyfox1988@gmail.com>
2015-11-02 18:43:08 -0600

    Allergies class written and functional.

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,15 +1,26 @@
 # exercism io allergies
 
 class Allergies(object):
+	''' Write a program that, given a person's allergy score, can tell them
+	whether or not they're allergic to a given item, and their full list of
+	allergies.
+
+	An allergy test produces a single numeric score which contains the
+	information about all the allergies the person has (that they were
+	tested for).
+
+	Note: I think in order to pass the extra credit, I'd need to do this in
+	binary. I'm submitting this since all tests pass and will work on the extra
+	credit test in the 2nd iteration.
+	'''
 
 	@property
 	def lst(self):
-		return self.allergies
+		return self.allergicTo
 
 	def __init__(self, allergyScore=0):
 		self.allergyScore = allergyScore
-		self.allergies = []
-
+		self.allergicTo = []
 		self.allergyMap = {
 			128: 'cats',
 			64: 'pollen',
@@ -24,18 +35,18 @@ class Allergies(object):
 		self._calc_allergies()
 
 	def _calc_allergies(self):
-		for food in reversed(sorted(self.allergyMap.keys())):
-			if self.allergyScore >= food:
-				self.allergies.append(self.allergyMap[food])
-				self.allergyScore =  self.allergyScore - food
+		for allergy in reversed(sorted(self.allergyMap.keys())):
+			if self.allergyScore >= allergy:
+				self.allergicTo.append(self.allergyMap[allergy])
+				self.allergyScore =  self.allergyScore - allergy
 				self._calc_allergies()
 
 
-	def is_allergic_to(self, food):
-		return (food in self.allergies)
+	def is_allergic_to(self, allergy):
+		return (allergy in self.allergicTo)
 
 
 if __name__ == '__main__':
-	allergies = Allergies(255)
+	allergies = Allergies(34)
 	print(len(allergies.lst))
 	print(allergies.lst)
