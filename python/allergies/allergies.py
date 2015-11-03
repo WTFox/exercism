83107d1b19a099cae5c5da65fa11112510cbbcf4
Anthony Fox <anthonyfox1988@gmail.com>
2015-11-02 18:15:19 -0600

    Allergies tests all pass

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,3 +1,4 @@
+# exercism io allergies
 
 class Allergies(object):
 
@@ -7,27 +8,34 @@ class Allergies(object):
 
 	def __init__(self, allergyScore=0):
 		self.allergyScore = allergyScore
-		
 		self.allergies = []
+
 		self.allergyMap = {
-			1: 'eggs',
-			2: 'peanuts',
-			4: 'shellfish',
-			8: 'strawberries',
-			16: 'tomatoes',
-			32: 'chocolate',
+			128: 'cats',
 			64: 'pollen',
-			128: 'cats'
+			32: 'chocolate',
+			16: 'tomatoes',
+			8: 'strawberries',
+			4: 'shellfish',
+			2: 'peanuts',
+			1: 'eggs'
 		}
 
-		workingNum = self.allergyScore
-		for food in self.allergyMap.keys():
-			if workingNum > food:
+		self._calc_allergies()
+
+	def _calc_allergies(self):
+		for food in reversed(sorted(self.allergyMap.keys())):
+			if self.allergyScore >= food:
 				self.allergies.append(self.allergyMap[food])
-				# workingNum = workingNum - food
-			
-		
+				self.allergyScore =  self.allergyScore - food
+				self._calc_allergies()
+
+
 	def is_allergic_to(self, food):
 		return (food in self.allergies)
-	
-		
+
+
+if __name__ == '__main__':
+	allergies = Allergies(255)
+	print(len(allergies.lst))
+	print(allergies.lst)
