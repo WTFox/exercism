69062da86174cfdc6587068bcdb132ca25ae8b10
WTFox <Anthonyfox1988@Gmail.com>
2015-11-05 22:02:33 -0600

    worked on atbash

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,21 +1,37 @@
+# exercism.io atbash_cipher file
+
+# http://stackoverflow.com/questions/31040525/insert-element-in-python-list-after-every-nth-element
 
 def encode(msg):
-    msg = msg.lower().strip()
-    plain = "abcdefghijklmnopqrstuvwxyz"
-    cipher = "zyxwvutsrqponmlkjihgfedcba"
+    encodedMsg = ''
+    cleansedMsg = [x for x in msg.lower().strip() if x.isalnum()]
+    tmap = dict(zip("abcdefghijklmnopqrstuvwxyz",
+                    "zyxwvutsrqponmlkjihgfedcba"))
 
-    trans = maketrans(plain, cipher)
-    return msg.translate(trans)
+    for index, letter in enumerate(cleansedMsg):
+        print(index, letter)
+
+        if letter.isalpha():
+            encodedMsg += tmap[letter]
+        elif letter.isdigit():
+            encodedMsg += letter
+
+    return encodedMsg
 
 
 def decode(msg):
-    msg.lower().strip()
-    plain = "zyxwvutsrqponmlkjihgfedcba"
-    cipher = "abcdefghijklmnopqrstuvwxyz"
+    output = []
+    tmap = dict(zip("zyxwvutsrqponmlkjihgfedcba",
+                    "abcdefghijklmnopqrstuvwxyz"))
+    for x in msg.lower().strip():
+        if x.isalpha():
+            output.append(tmap[x])
+        elif x.isdigit():
+            output.append(x)
 
-    trans = maketrans(plain, cipher)
-    return msg.lower().translate(trans)
+    return ''.join(output).strip()
 
 
 if __name__ == '__main__':
-    encode("Testing, 1 2 3, testing.")
+    print(encode("Truth is fiction."))
+    print("gifgs rhurx grlm")
