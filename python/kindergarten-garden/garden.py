896aa00051eb0cec6ae631544d3343d5ad758a9a
WTFox <Anthonyfox1988@Gmail.com>
2015-11-06 22:53:33 -0600

    finished kindegarten garden. fetched grade-school

--- python/kindergarten-garden/garden.py
+++ python/kindergarten-garden/garden.py
@@ -1,7 +1,5 @@
 # exercism io Kindegarten Garden
 
-from pprint import pprint
-
 class Garden(object):
     _students = ["Alice", "Bob", "Charlie", "David",
                 "Eve", "Fred", "Ginny", "Harriet",
@@ -11,40 +9,33 @@ class Garden(object):
     def __init__(self, plants, students=_students):
         self.firstRow, self.secondRow = plants.split('\n')
         self._students = students
-        self.myMap = {}
 
+        self.myMap = {}
         index = 0
         for student in self.students:
-            print(student)
-            try:
-                plantList = [self.firstRow[index],
-                            self.firstRow[index+1],
-                            self.secondRow[index],
-                            self.secondRow[index+1]]
-            except IndexError:
-                continue
-            self.myMap[student] = plantList
-            index += 2
-
-        print(self.myMap)
+            if len(self.firstRow) - index > 1:
+                plantList = [
+                    self.firstRow[index],
+                    self.firstRow[index+1],
+                    self.secondRow[index],
+                    self.secondRow[index+1]
+                ]
+                self.myMap[student] = plantList
+                index += 2
         return
 
     @property
     def students(self):
         return sorted(self._students)
 
-    def plants(self, name):
+    def plants(self, student):
         translation = {
             'R': 'Radishes',
             'C': 'Clover',
             'G': 'Grass',
             'V': 'Violets'
         }
-
-        print([translation[x] for x in self.myMap[name]])
-        return [translation[x] for x in self.myMap[name]]
-
+        return [translation[x] for x in self.myMap[student]]
 
 if __name__ == '__main__':
     garden = Garden("VVCCGG\nVVCCGG")
-    garden.plants("Bob")
