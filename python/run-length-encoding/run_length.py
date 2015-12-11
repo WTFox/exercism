505005702360aa086fb0c98989dee22eaa6d0ee1
unknown <afox@TCGSP3-036410.thecrichtongroup.com>
2015-12-11 15:49:42 -0600

    Doing horribly on run-length-encoding

--- python/run-length-encoding/run_length.py
+++ python/run-length-encoding/run_length.py
@@ -1,8 +1,22 @@
-# -*- coding: utf-8 -*-
 __author__ = 'afox'
 
+
 def encode(dataset):
-    pass
+    print([x for x in dataset])
+    output = []
+    temp1 = list(dataset)
+    temp = []
+    for i, v in enumerate(dataset):
+        if v == dataset[i-1]:
+            temp.append(temp1.pop(0))
+        else:
+            continue
+
+    print(temp)
 
 def decode(dataset):
     pass
+
+
+if __name__ == '__main__':
+    encode('AABBBCCCC')
