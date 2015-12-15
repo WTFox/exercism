30c4d5a532aaa01c81597edc46fbece08e5dc750
WTFox <Anthonyfox1988@Gmail.com>
2015-12-14 20:16:31 -0600

    First draft of encode working.

--- python/run-length-encoding/run_length.py
+++ python/run-length-encoding/run_length.py
@@ -1,22 +1,56 @@
 __author__ = 'afox'
 
+import re
 
 def encode(dataset):
-    print([x for x in dataset])
-    output = []
-    temp1 = list(dataset)
-    temp = []
-    for i, v in enumerate(dataset):
-        if v == dataset[i-1]:
-            temp.append(temp1.pop(0))
-        else:
-            continue
+    """
+    This is working.. but it ain't pretty.
 
-    print(temp)
+    :param dataset:
+    :return:
+
+    """
+    counter = 1
+    output = []
+
+    for i, v in enumerate(dataset):
+        if hasNext(i, dataset):
+            nextChar = dataset[i + 1]
+            if v == nextChar:
+                counter += 1
+
+            elif v != nextChar:
+                if counter == 1:
+                    output.append("{}{}".format('', v))
+                else:
+                    output.append("{}{}".format(counter, v))
+                counter = 1
+
+        else:
+            if v == dataset[i - 1]:
+                if counter == 1:
+                    output.append("{}{}".format('', v))
+                else:
+                    output.append("{}{}".format(counter, v))
+
+            else:
+                if counter == 1:
+                    output.append("{}{}".format('', v))
+                else:
+                    output.append("{}{}".format(counter, v))
+
+    return ''.join(output)
+
 
 def decode(dataset):
     pass
 
 
+def hasNext(index, obj):
+    return (len(obj) - index) > 1
+
+
 if __name__ == '__main__':
-    encode('AABBBCCCC')
+    print(encode('AABBBCCCC'))
+    # print(encode('WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB'))
+
