3f5e5ca1222ec55b6db9bca4c60290884edcac04
WTFox <Anthonyfox1988@Gmail.com>
2016-03-06 14:23:39 -0600

    Pass all tests for acronym

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,4 +1,13 @@
-__author__ = 'anthonyfox'
+import re
 
-def abbreviate():
-    pass
+
+def abbreviate(sen):
+    acronym = []
+    match = re.findall(r'\w+', sen)
+    for word in match:
+        acronym.append(word[0].upper())
+        if not word.isupper():
+            inter_word_caps = re.findall(r'[A-Z]', word[1:-1])
+            acronym.append(''.join(inter_word_caps))
+
+    return ''.join(acronym)
