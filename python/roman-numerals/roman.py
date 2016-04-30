b9a5b613d4247811d4672b0c80a7cf0fad696d48
WTFox <Anthonyfox1988@Gmail.com>
2016-04-30 10:17:04 -0500

    Submit second iter of roman.py.

--- python/roman-numerals/roman.py
+++ python/roman-numerals/roman.py
@@ -1,35 +1,12 @@
-from collections import OrderedDict
+def numeral(num):
 
-__author__ = 'anthonyfox'
+    """ Convert an integer to a Roman numeral. """
+    ints = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+    nums = ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
+    result = []
+    for i in range(len(ints)):
+        count = int(num / ints[i])
+        result.append(nums[i] * count)
+        num -= ints[i] * count
 
-
-def numeral(arabic_num):
-    output = []
-    working_num = arabic_num
-    while working_num:
-        out, sub = get_num(working_num)
-        working_num -= sub
-        output.append(out)
-
-    return ''.join(output)
-
-
-def get_num(num):
-    num_map = OrderedDict()
-    num_map['M'] = range(1000, 4000)
-    num_map['CM'] = range(900, 1000)
-    num_map['D'] = range(500, 900)
-    num_map['CD'] = range(400, 500)
-    num_map['C'] = range(100, 400)
-    num_map['XC'] = range(90, 100)
-    num_map['L'] = range(50, 90)
-    num_map['XL'] = range(40, 50)
-    num_map['X'] = range(10, 40)
-    num_map['IX'] = range(9, 10)
-    num_map['V'] = range(5, 9)
-    num_map['IV'] = range(4, 5)
-    num_map['I'] = range(1,4)
-
-    for k, v in num_map.items():
-        if num in v:
-            return k, v[0]
+    return ''.join(result)
