a988ca3c694c30bfc8d7488e6327bceed97208f4
Anthony Fox <anthonyfox1988@gmail.com>
2016-05-17 19:26:27 -0500

    Finished Luhn. Fetched ETL

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,55 +1,43 @@
-__author_ = 'afox'
-
-
 class Luhn:
+    """
+        Write a program that can take a number and determine whether or not it
+        is valid per the Luhn formula.
+    """
+
     def __init__(self, num):
         self.num = num
 
     def addends(self):
-        rlist = reversed(list(str(self.num)))
-        output = []
-        for i, n in enumerate(rlist, start=1):
-            n = int(n)
-            if i % 2 == 0:
-                e = n * 2
-                if e >= 10:
-                    e -= 9
-                output.append(e)
-            else:
-                output.append(n)
-
-        return [x for x in reversed(output)]
+        return self._calc_addends(self.num, start=1)
 
     def checksum(self):
-        output = self.addends()
-        return sum([int(x) for x in output])
+        return sum([int(x) for x in self.addends()])
 
     def is_valid(self):
         return self.checksum() % 10 == 0
 
     @staticmethod
-    def create(num):
-        # The sum is 6+5+7 = 18. As 18 % 10 = 8
-        # (10 - 8) % 10 = 2
-        # 2 is the digit checksum control.
+    def _process_num(num):
+        out = num * 2
+        if out <= 9:
+            return out
+        return out - 9
 
-        rlist = reversed(list(str(num)))
+    @classmethod
+    def create(cls, num):
+        addend = cls._calc_addends(num, start=0)
+        mod = sum([int(x) for x in addend]) % 10
+        check_digit = (10 - mod) % 10
+        return int("{}{}".format(num, check_digit))
+
+    @classmethod
+    def _calc_addends(cls, num, start):
         output = []
-        for i, n in enumerate(rlist, start=0):
+        for i, n in enumerate(str(num)[::-1], start=start):
             n = int(n)
             if i % 2 == 0:
-                e = n * 2
-                if e >= 10:
-                    e -= 9
-                output.append(e)
+                output.append(cls._process_num(n))
             else:
                 output.append(n)
 
-        num2 = sum([int(x) for x in output])
-        mod = num2 % 10
-        check_digit = (10 - mod) % 10
-
-        return int("{}{}".format(num, check_digit))
-
-
-Luhn.create(123)
+        return [x for x in output[::-1]]
