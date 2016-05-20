2ee38762f443bd94f43c95c81d1129b9c5364f84
Anthony Fox <anthonyfox1988@gmail.com>
2016-05-20 15:48:18 -0500

    Start pig_latin.py

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,6 +1,16 @@
-def translate():
-    pass
+def translate(phrase):
+    translated = []
+    vowels = 'a e i o u x yt'.split()
+    special_cases = 'qu ch sq'.split()
 
+    for word in phrase.split():
+        if word[0] in vowels:
+            translated.append("{}{}".format(word, "ay"))
+        elif word[:2] in special_cases:
+            translated.append("{}{}{}".format(word[2:], word[:2], "ay"))
+        else:
+            translated.append("{}{}{}".format(word[1:], word[0], "ay"))
 
-if __name__ == '__main__':
-    translate()
+    return ' '.join(translated)
+
+translate('apple')
