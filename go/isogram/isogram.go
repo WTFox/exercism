a38104414f3c014af7de2d4c090eff88a2b4b1c0
A. Fox <anthonyfox1988@gmail.com>
2019-03-15 16:10:55 -0700

    A much better solution to isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,27 +1,18 @@
 package isogram
 
-import "strings"
-
-func sliceContains(collection []string, value string) bool {
-	for _, val := range collection {
-		if val == value {
-			return true
-		}
-	}
-	return false
-}
+import "unicode"
 
 // IsIsogram determines if a word is an isogram
 func IsIsogram(word string) bool {
-	var seen []string
-	for _, letter := range strings.Split(strings.ToLower(word), "") {
-		if strings.Contains("- ", letter) {
+	var seen = make(map[rune]bool)
+	for _, r := range word {
+		if !unicode.IsLetter(r) {
 			continue
 		}
-		if sliceContains(seen, letter) {
+		if _, ok := seen[unicode.ToUpper(r)]; ok {
 			return false
 		}
-		seen = append(seen, letter)
+		seen[unicode.ToUpper(r)] = true
 	}
 	return true
 }
