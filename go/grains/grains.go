c433a88352a89f859ab2dd2f45d046d9e77f6bf5
A. Fox <anthonyfox1988@gmail.com>
2019-04-16 09:05:19 -0700

    Add grains.go

---
+++ go/grains/grains.go
@@ -0,0 +1,19 @@
+package grains
+
+import (
+	"errors"
+	"math"
+)
+
+// Square takes
+func Square(input int) (uint64, error) {
+	if input < 1 || input > 64 {
+		return 0, errors.New("invalid number")
+	}
+	return uint64(math.Pow(2, float64(input-1))), nil
+}
+
+// Total takes
+func Total() uint64 {
+	return 18446744073709551615
+}
