6724aea11000cdff05b649f322005004110767b7
A. Fox <anthonyfox1988@gmail.com>
2021-02-28 12:34:00 -0800

    4 tests failing on tree_building

---
+++ go/tree-building/tree_building.go
@@ -0,0 +1,97 @@
+package tree
+
+import (
+	"errors"
+	"sort"
+)
+
+// Record represents a record
+type Record struct {
+	ID     int
+	Parent int
+}
+
+// Node represents a node in a tree structure
+type Node struct {
+	ID       int
+	Children []*Node
+}
+
+func (n *Node) appendChild(node *Node) {
+	n.Children = append(n.Children, node)
+	n.sortChildren()
+}
+
+func (n *Node) sortChildren() {
+	sort.SliceStable(n.Children, func(i, j int) bool {
+		return n.Children[i].ID < n.Children[j].ID
+	})
+}
+
+func findNodeByID(id int, node *Node) (*Node, bool) {
+	if id == node.ID {
+		return node, true
+	}
+
+	if len(node.Children) > 0 {
+		for _, child := range node.Children {
+			if node, ok := findNodeByID(id, child); ok {
+				return node, true
+			}
+		}
+	}
+	return nil, false
+}
+
+func validateRecord(record Record, rootNode *Node) error {
+	if record.ID == rootNode.ID && record.Parent >= 0 {
+		return errors.New("root node can't have parent")
+	}
+
+	if record.ID == rootNode.ID {
+		return errors.New("can't have multiple roots")
+	}
+
+	if record.Parent > record.ID {
+		return errors.New("parent ID shouldn't be higher than this ID")
+	}
+
+	if record.Parent == record.ID {
+		return errors.New("ID's of parent and Node can't be equal")
+	}
+	return nil
+}
+
+// Build takes records and returns the root node of a tree representation
+func Build(records []Record) (*Node, error) {
+	if len(records) == 0 {
+		return nil, nil
+	}
+
+	sort.SliceStable(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
+
+	var rootNode *Node
+
+	for idx, r := range records {
+		if idx == 0 {
+			rootNode = &Node{
+				ID: r.ID,
+			}
+			continue
+		}
+
+		if err := validateRecord(r, rootNode); err != nil {
+			return nil, err
+		}
+
+		if node, ok := findNodeByID(r.Parent, rootNode); ok {
+			node.appendChild(&Node{
+				ID: r.ID,
+			})
+		}
+	}
+
+	return rootNode, nil
+}
