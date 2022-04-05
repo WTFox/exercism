6f1210fe3fb3f298c574c19c269759f8e869d1d0
A. Fox <anthonyfox1988@gmail.com>
2022-04-04 21:42:53 -0700

    refactorings

--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -15,20 +15,22 @@ export class DnDCharacter {
     this.charisma = DnDCharacter.generateAbilityScore();
   }
 
-  public get hitpoints() {
-    return Math.floor(10 + DnDCharacter.getModifierFor(this.constitution));
-  }
-
   public static generateAbilityScore(): number {
+    const rollSixSidedDice = () => Math.floor(Math.random() * 6) + 1;
+
     return new Array(4)
       .fill(0)
-      .map(() => Math.floor(Math.random() * (7 - 1)) + 1)
+      .map(rollSixSidedDice)
       .sort((a, b) => a - b)
       .slice(1, 4)
-      .reduce((prev, curr) => prev + curr, 1);
+      .reduce((prev, curr) => prev + curr);
   }
 
   public static getModifierFor(abilityValue: number): number {
     return Math.floor((abilityValue - 10) / 2);
+  }
+
+  public get hitpoints() {
+    return Math.floor(10 + DnDCharacter.getModifierFor(this.constitution));
   }
 }
