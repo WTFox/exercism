d97b7f8f6b048562b97443e620506c5977a6af59
A. Fox <anthonyfox1988@gmail.com>
2022-04-14 15:28:35 -0700

    first pass at linked list

--- typescript/linked-list/linked-list.ts
+++ typescript/linked-list/linked-list.ts
@@ -1,25 +1,95 @@
+interface INode<TElement> {
+  data?: TElement
+  next?: INode<TElement>
+  previous?: INode<TElement>
+}
+
+function createEmptyNode<TElement>(): INode<TElement> {
+  return { data: undefined, next: undefined, previous: undefined }
+}
+
 export class LinkedList<TElement> {
-  public push(element: unknown) {
-    throw new Error("Remove this statement and implement this function");
+  private head: INode<TElement>
+  private tail: INode<TElement>
+  private _count: number
+
+  constructor() {
+    this._count = 0
+    this.head = createEmptyNode()
+    this.tail = this.head
   }
 
-  public pop(): unknown {
-    throw new Error("Remove this statement and implement this function");
+  public count(): number {
+    return this._count
   }
 
-  public shift(): unknown {
-    throw new Error("Remove this statement and implement this function");
+  public push(element: TElement): void {
+    if (this._count === 0) {
+      this.head = { data: element }
+    } else if (this._count === 1) {
+      this.tail = { data: element, previous: this.head }
+      this.head.next = this.tail
+    } else {
+      const newNode = { data: element, previous: this.tail }
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+    this._count++
   }
 
-  public unshift(element: unknown) {
-    throw new Error("Remove this statement and implement this function");
+  public unshift(element: TElement): void {
+    if (this._count === 0) {
+      this.head = { data: element }
+    } else {
+      const currentHead = this.head
+      this.head = { data: element, next: currentHead }
+      currentHead.previous = this.head
+      this.tail = currentHead
+    }
+    this._count++
   }
 
-  public delete(element: unknown) {
-    throw new Error("Remove this statement and implement this function");
+  public pop(): TElement | undefined {
+    const lastElement = this.tail.data === undefined ? this.head : this.tail
+    const data = lastElement.data
+    if (lastElement.previous !== undefined) {
+      this.tail = lastElement.previous
+    }
+    this._count--
+    return data
   }
 
-  public count(): unknown {
-    throw new Error("Remove this statement and implement this function");
+  public shift(): TElement | undefined {
+    const currentHead = this.head
+    const val = currentHead.data
+
+    if (currentHead.next !== undefined) {
+      this.head = currentHead.next
+    }
+
+    this._count--
+    return val
+  }
+
+  public delete(element: TElement): void {
+    let node = this.head
+    while (node.data !== undefined) {
+      if (node.data === element) {
+        if (this._count === 1) {
+          this.head = createEmptyNode()
+          this.tail = this.head
+          this._count = 0
+          return
+        } else if (node.previous) {
+          node.previous = node.next
+          this._count--
+          return
+        }
+      }
+      if (node.next === undefined) {
+        break
+      }
+      node = node.next
+    }
   }
 }
