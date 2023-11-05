2c7cbf44aec32b3d7818baea9eb8dbd1303a2130
A. Fox <anthonyfox1988@gmail.com>
2023-11-05 03:39:38 +0000

    iono

--- python/leap/year.py
+++ python/leap/year.py
@@ -1,13 +1,10 @@
 ## exercism.io leap challenge
 
-def is_leap_year(year):
-	'''
-	on every year that is evenly divisible by 4
-	  except every year that is evenly divisible by 100
-	    unless the year is also evenly divisible by 400
-	'''
 
-	if year % 4 == 0:
-		return not year % 100 == 0 or year % 400 == 0
-			
-	return False
+def is_leap_year(year):
+    """Leap years occur on every year that is evenly divisible by 4 except every year that is evenly divisible by 100 unless the year is also evenly divisible by 400"""
+
+    if year % 4 == 0:
+        return not year % 100 == 0 or year % 400 == 0
+
+    return False
--- rust/hello-world/src/lib.rs
+++
@@ -1,5 +0,0 @@
-// &'static is a "lifetime specifier", something you'll learn more about later
-pub fn hello() -> &'static str {
-    "Hello, World!"
-}
-
