04e855b8318a88a04b29118bd12f25790fb0178e
A. Fox <anthonyfox1988@gmail.com>
2023-11-06 16:05:06 -0800

    improvements suggested by ChatGPT

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,58 +1,47 @@
 fn count_surrounding_mines(row_i: usize, col_i: usize, minefield: &[&str]) -> usize {
-    let mut count = 0;
-
     #[rustfmt::skip]
     let adjacent_offsets = [
         (-1, -1), (-1, 0), (-1, 1),
-        (0, -1),           (0, 1),
-        (1, -1),  (1, 0),  (1, 1),
+        (0, -1), (0, 1),
+        (1, -1), (1, 0), (1, 1),
     ];
 
-    for &(row_offset, col_offset) in adjacent_offsets.iter() {
-        let adjacent_row = row_i as isize + row_offset;
-        let adjacent_column = col_i as isize + col_offset;
+    adjacent_offsets
+        .iter()
+        .filter_map(|&(row_offset, col_offset)| {
+            let adjacent_row = row_i as isize + row_offset;
+            let adjacent_column = col_i as isize + col_offset;
 
-        if adjacent_row < 0 || adjacent_row >= minefield.len() as isize {
-            continue;
-        }
-        if adjacent_row < 0 || adjacent_column >= minefield[0].len() as isize {
-            continue;
-        }
-
-        let adjacent_cell = minefield[adjacent_row as usize]
-            .chars()
-            .nth(adjacent_column as usize)
-            .unwrap_or(' ');
-
-        if adjacent_cell == '*' {
-            count += 1;
-        }
-    }
-
-    count
+            if adjacent_row >= 0
+                && adjacent_row < minefield.len() as isize
+                && adjacent_column >= 0
+                && adjacent_column < minefield[0].len() as isize
+            {
+                return minefield[adjacent_row as usize]
+                    .chars()
+                    .nth(adjacent_column as usize);
+            } else {
+                None
+            }
+        })
+        .filter(|&c| c == '*')
+        .count()
 }
 
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
     if minefield.is_empty() {
-        return vec![];
-    }
-
-    let expected_length = minefield[0].len();
-    for row in minefield.iter() {
-        if row.len() != expected_length {
-            panic!("Invalid minefield");
-        }
+        return Vec::new();
     }
 
     let mut result = Vec::with_capacity(minefield.len());
     for (row_i, row) in minefield.iter().enumerate() {
-        let mut new_row = String::from("");
-        for (cell_i, cell) in row.chars().enumerate() {
-            if cell != '*' {
+        let mut new_row = String::with_capacity(row.len());
+        for (cell_i, &cell) in row.as_bytes().iter().enumerate() {
+            if cell != b'*' {
                 let count = count_surrounding_mines(row_i, cell_i, minefield);
                 match count {
                     0 => new_row.push(' '),
-                    _ => new_row.push(count.to_string().chars().next().unwrap()),
+                    _ => new_row.push(char::from_digit(count as u32, 10).unwrap()),
                 }
             } else {
                 new_row.push('*');
