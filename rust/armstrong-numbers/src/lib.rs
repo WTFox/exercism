c153e5e4c949c52a755a1cba4b6aaad8276cb667
A. Fox <anthonyfox1988@gmail.com>
2023-11-07 08:46:32 -0800

    rust/armstrong-numbers #2

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,6 +1,10 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    let num_str = num.to_string();
-    num_str.chars().map(|c| c.to_digit(10)).fold(0, |acc, x| {
-        acc + x.unwrap().pow(num_str.len() as u32) as u64
-    }) == (num as u64)
+    let digits: Vec<_> = num.to_string().chars().map(|c| c.to_digit(10)).collect();
+    let length = digits.len() as u32;
+    let sum = digits
+        .iter()
+        .map(|x| x.unwrap().pow(length) as u64)
+        .sum::<u64>();
+
+    sum == (num as u64)
 }
