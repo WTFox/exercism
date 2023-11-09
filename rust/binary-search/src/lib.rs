b715560bd3154545f898d631ab184f087e54b877
A. Fox <anthonyfox1988@gmail.com>
2023-11-09 11:08:14 -0800

    rust/binary-search #1

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,5 +1,27 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    todo!(
-        "Using the binary search algorithm, find the element '{key}' in the array '{array:?}' and return its index."
-    );
+use std::cmp::Ordering;
+
+pub fn find<T: Ord>(array: &[T], key: T) -> Option<usize> {
+    binary_search(array, key, 0)
+}
+
+fn binary_search<T: Ord>(array: &[T], key: T, _offset: i32) -> Option<usize> {
+    if array.is_empty() {
+        return None;
+    }
+
+    if array.len() == 1 {
+        return if array[0] == key {
+            Some(_offset as usize)
+        } else {
+            None
+        };
+    }
+
+    let pivot = array.len() / 2;
+    let (left, right) = array.split_at(pivot);
+    match key.cmp(&right[0]) {
+        Ordering::Equal => Some((_offset + pivot as i32) as usize),
+        Ordering::Less => binary_search(left, key, _offset),
+        Ordering::Greater => binary_search(right, key, _offset + pivot as i32),
+    }
 }
