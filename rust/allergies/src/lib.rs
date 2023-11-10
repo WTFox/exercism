59d48787884ed40be9408854ff203dcb3a702719
A. Fox <anthonyfox1988@gmail.com>
2023-11-10 10:21:21 -0800

    rust/allergies #1

--- rust/allergies/src/lib.rs
+++ rust/allergies/src/lib.rs
@@ -1,6 +1,4 @@
-pub struct Allergies;
-
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Allergen {
     Eggs,
     Peanuts,
@@ -12,16 +10,40 @@ pub enum Allergen {
     Cats,
 }
 
+impl Allergen {
+    pub fn to_vec() -> Vec<Allergen> {
+        vec![
+            Allergen::Eggs,
+            Allergen::Peanuts,
+            Allergen::Shellfish,
+            Allergen::Strawberries,
+            Allergen::Tomatoes,
+            Allergen::Chocolate,
+            Allergen::Pollen,
+            Allergen::Cats,
+        ]
+    }
+}
+
+pub struct Allergies {
+    score: u32,
+}
+
 impl Allergies {
     pub fn new(score: u32) -> Self {
-        todo!("Given the '{score}' score, construct a new Allergies struct.");
+        Allergies { score }
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        todo!("Determine if the patient is allergic to the '{allergen:?}' allergen.");
+        self.allergies().contains(allergen)
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        todo!("Return the list of allergens contained within the score with which the Allergies struct was made.");
+        Allergen::to_vec()
+            .iter()
+            .enumerate()
+            .filter(|(i, _)| 1 << i & self.score != 0)
+            .map(|(_, a)| a.clone())
+            .collect::<Vec<_>>()
     }
 }
