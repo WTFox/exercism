4aa03617c33595a63d3916f705dc39cd74292042
A. Fox <anthonyfox1988@gmail.com>
2023-11-11 23:35:14 +0000

    rust/grains #1

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,10 @@
 pub fn square(s: u32) -> u64 {
-    todo!("grains of rice on square {s}");
+    if s == 0 || s > 64 {
+        panic!("Square must be between 1 and 64")
+    }
+    (1..s).fold(1, |acc, _| acc * 2)
 }
 
 pub fn total() -> u64 {
-    todo!();
+    (1..=64).map(square).sum()
 }
