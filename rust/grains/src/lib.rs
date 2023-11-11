99447938ac25be1449b2b3612623f463e214b85a
A. Fox <anthonyfox1988@gmail.com>
2023-11-11 23:38:57 +0000

    use match stmt

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn square(s: u32) -> u64 {
-    if s == 0 || s > 64 {
-        panic!("Square must be between 1 and 64")
+    match s {
+        1..=64 => 2_u64.pow(s - 1),
+        _ => panic!("Square must be between 1 and 64"),
     }
-    (1..s).fold(1, |acc, _| acc * 2)
 }
 
 pub fn total() -> u64 {
